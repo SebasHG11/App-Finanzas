989747b5399dbcd7d49ee8893307b5f6c9b14982
SebasHG11 <sebashiguitag@gmail.com>
2024-08-21 11:10:43 -0500

    Actualizo botones

--- src/components/BtnCrearCategoriaIngresos/index.tsx
+++ src/components/BtnCrearCategoriaIngresos/index.tsx
@@ -1,7 +1,7 @@
 export const BtnCrearCategoriaIngresos = () =>{
     return(
-        <span className="bg-yellow-500 rounded-md px-4 py-2 text-white cursor-pointer">
-            BtnCrearCategoriaIngresos
+        <span className="bg-yellow-500 rounded-md px-4 py-2 text-white cursor-pointer font-bold">
+            Agregar Categoria Para Ingresos
         </span>
     );
 }
--- src/components/BtnCrearIngreso/index.tsx
+++ src/components/BtnCrearIngreso/index.tsx
@@ -1,7 +1,7 @@
 export const BtnCrearIngreso = ():JSX.Element =>{
     return(
-        <span className="bg-yellow-500 rounded-md px-4 py-2 text-white cursor-pointer">
-            BtnCrearIngreso
+        <span className="bg-yellow-500 rounded-md px-4 py-2 text-white cursor-pointer font-bold">
+            Agregar Ingreso
         </span>
     );
 }
