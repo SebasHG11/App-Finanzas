b89b8029a93aa39a427773bd582aeb97de95c219
SebasHG11 <sebashiguitag@gmail.com>
2024-08-21 08:39:59 -0500

    Agrego funcion para formatear fecha

--- src/context/FinanzaContext.tsx
+++ src/context/FinanzaContext.tsx
@@ -1,11 +1,14 @@
-import { createContext } from "react";
+import { createContext, Dispatch, SetStateAction, useState } from "react";
 
 type Props ={
     children: React.ReactNode
 }
 
 type FinanzaContextType = {
-    formatMonto: (amount: number) => string
+    formatMonto: (amount: number) => string, 
+    useTotalIngresos: number,
+    setUseTotalIngresos: Dispatch<SetStateAction<number>>,
+    formatFecha: (fecha: string) => string
 }
 
 export const FinanzaContext = createContext<FinanzaContextType | undefined>(undefined);
@@ -19,9 +22,37 @@ export const AppProvider: React.FC<Props> = ({ children }) =>{
         return amount.toLocaleString('es-ES')
     }
 
+    const [useTotalIngresos, setUseTotalIngresos] = useState<number>(0);
+
+    const formatFecha = (fecha: string): string => {
+        if (!fecha) {
+            return 'Fecha inválida';
+        }
+        
+        const date = new Date(fecha);
+    
+        // Verificar si la fecha es válida
+        if (isNaN(date.getTime())) {
+            return 'Formato de fecha inválido';
+        }
+    
+        // Obtener componentes de la fecha
+        const day = date.getDate().toString().padStart(2, '0');
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const year = date.getFullYear();
+        const hours = date.getHours().toString().padStart(2, '0');
+        const minutes = date.getMinutes().toString().padStart(2, '0');
+    
+        // Formatear la fecha como 'DD/MM/YYYY HH:mm'
+        return `${day}/${month}/${year} ${hours}:${minutes}`;
+    };
+
     return(
         <FinanzaContext.Provider value={{
-            formatMonto
+            formatMonto,
+            useTotalIngresos,
+            setUseTotalIngresos,
+            formatFecha
         }}>
             {children}
         </FinanzaContext.Provider>
