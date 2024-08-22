af96d72bdee93c9e4760dfb7fceba514f065746d
SebasHG11 <sebashiguitag@gmail.com>
2024-08-22 13:24:38 -0500

    Cambio estilos en las card

--- src/components/CardGasto/index.tsx
+++ src/components/CardGasto/index.tsx
@@ -9,15 +9,15 @@ export const CardGasto = ({ gasto }: Props): JSX.Element => {
   const context = useContext(FinanzaContext);
 
   return (
-    <div className="p-4 w-60 grid place-items-center border-2 border-red-600 rounded-lg">
+    <div className="p-4 w-60 grid place-items-center border-2 bg-red-100 border-red-600 rounded-lg">
       <h4 className="font-semibold text-xl text-center">
         {gasto.concepto}
       </h4>
       <p className="font-light">Monto</p>
-      <p>
+      <p className="font-semibold text-lg">
         {context?.formatMonto(gasto.monto)}$
       </p>
-      <p className="font-extralight">
+      <p className="roboto-condensed font-extralight">
         {context?.formatFecha(gasto.fecha)}
       </p>
       <p className="font-bold text-red-800">
--- src/components/CardIngreso/index.tsx
+++ src/components/CardIngreso/index.tsx
@@ -9,15 +9,15 @@ export const CardIngreso = ({ ingreso }: Props): JSX.Element => {
     const context = useContext(FinanzaContext);
 
     return(
-        <div className="p-4 w-60 grid place-items-center border-2 border-green-600 rounded-lg">
+        <div className="p-4 w-60 grid place-items-center border-2 bg-green-100 border-green-600 rounded-lg">
             <h4 className="font-semibold text-xl text-center">
                 {ingreso.concepto}
             </h4>
             <p className="font-light">Monto</p>
-            <p>
+            <p className="font-semibold text-lg">
                 {context?.formatMonto(ingreso.monto)}$    
             </p>
-            <p className="font-extralight">
+            <p className="roboto-condensed font-extralight">
                 {context?.formatFecha(ingreso.fecha)}
             </p>
             <p className="font-bold text-green-800">
