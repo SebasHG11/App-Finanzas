6a71eea42377f03e828093564d0ff990bf86f285
SebasHG11 <sebashiguitag@gmail.com>
2024-08-22 10:40:00 -0500

    Agrego helper para realizar peticiones GET

---
+++ src/helpers/fetchData.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosError } from "axios";
+import { useEffect, useState } from "react";
+
+export const fetchData = <T>(url: string) => {
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<AxiosError | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get<T>(url);
+        setData(response.data);
+      } catch (error) {
+        setError(error as AxiosError);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getData();
+  }, [url]);
+
+  return {
+    data,
+    error,
+    loading
+  };
+}
